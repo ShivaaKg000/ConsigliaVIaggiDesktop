6719545f02e7eb46bf996dc1d1a01daecf3afebb
surfmast3r <surfmast3r@live.it>
2020-04-17 16:37:08 +0200

    Aggiunta ReviewDetailView

---
+++ consiglia-viaggi-desktop/src/main/java/consiglia/viaggi/desktop/view/ReviewDetailView.java
@@ -0,0 +1,34 @@
+package consiglia.viaggi.desktop.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class ReviewDetailView {
+
+    @FXML
+    private Label username_label;
+
+    @FXML
+    private Text username_text;
+
+    @FXML
+    private Label accommodation_label;
+
+    @FXML
+    private Text accommodation_text;
+
+    @FXML
+    private Label address_label;
+
+    @FXML
+    private Text address_text;
+
+    @FXML
+    private Label rating_label;
+
+    @FXML
+    private Text rating_text;
+
+}
+
