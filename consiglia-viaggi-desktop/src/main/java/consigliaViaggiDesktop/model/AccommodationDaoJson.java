fa2be83f587451e2c41c3d319c805fa03abef47c
surfmast3r <surfmast3r@live.it>
2020-05-04 15:12:28 +0200

    Added class AccommodationDaoJson

---
+++ consiglia-viaggi-desktop/src/main/java/consigliaViaggiDesktop/model/AccommodationDaoJson.java
@@ -0,0 +1,21 @@
+package consigliaViaggiDesktop.model;
+
+import java.util.List;
+
+public class AccommodationDaoJson implements AccommodationDao {
+
+	@Override
+	public List<Accommodation> getAccommodationList(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Accommodation getAccommodationById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+
+}
