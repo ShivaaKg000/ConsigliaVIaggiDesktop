4d99e0e3f29076150933f31ccea912fd82209274
surfmast3r <surfmast3r@live.it>
2020-05-08 19:29:28 +0200

    Added GMapsFx

--- consiglia-viaggi-desktop/src/main/java/consigliaViaggiDesktop/view/AccommodationDetailView.java
+++ consiglia-viaggi-desktop/src/main/java/consigliaViaggiDesktop/view/AccommodationDetailView.java
@@ -1,5 +1,8 @@
 package consigliaViaggiDesktop.view;
 
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.*;
 import consigliaViaggiDesktop.controller.ViewAccommodationController;
 import consigliaViaggiDesktop.model.Accommodation;
 import consigliaViaggiDesktop.model.Category;
@@ -12,20 +15,21 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 
-import java.awt.event.ActionEvent;
-
-public class AccommodationDetailView {
+public class AccommodationDetailView implements MapComponentInitializedListener {
 
 	@FXML private Text text_id;
 	@FXML private Text text_name;
-	@FXML private Text text_description;
-	@FXML private Text text_path;
+	@FXML private TextArea text_description;
+	//@FXML private Text text_path;
 	@FXML private Text text_rating;
 	@FXML private ComboBox<Category> choice_category;
 	@FXML private ComboBox<Subcategory> choice_subcategory;
-	
+	@FXML private GoogleMapView mapView;
+
+	private GoogleMap map;
 	
 	private int accommodationId;
 	private ViewAccommodationController viewAccommodationController;
@@ -41,9 +45,11 @@ public class AccommodationDetailView {
     	this.viewAccommodationController=viewAccommodationController;
 		
 	}
-	
+
 	public void initialize(){
-		
+
+		mapView.addMapInializedListener(this);
+
 		choice_category.setItems(category_list);
 		choice_subcategory.setItems(subcategory_list);
 
@@ -86,7 +92,7 @@ public class AccommodationDetailView {
 				text_id.setText(String.valueOf(accommodation.getId()));
 				text_name.setText(accommodation.getName());
 				text_description.setText(accommodation.getDescription());
-				text_path.setText(accommodation.getLogoUrl());
+				//text_path.setText(accommodation.getLogoUrl());
 				text_rating.setText(String.valueOf(accommodation.getRating()));
 				choice_category.setValue(accommodation.getCategory());
 				subcategory_list.addAll(dynamicSubCategoryChoice(accommodation.getCategory()));
@@ -119,7 +125,58 @@ public class AccommodationDetailView {
 	  viewAccommodationController.goBack();
     }
 
-    	
-    
 
+	@Override
+	public void mapInitialized() {
+		LatLong joeSmithLocation = new LatLong(47.6197, -122.3231);
+		LatLong joshAndersonLocation = new LatLong(47.6297, -122.3431);
+		LatLong bobUnderwoodLocation = new LatLong(47.6397, -122.3031);
+		LatLong tomChoiceLocation = new LatLong(47.6497, -122.3325);
+		LatLong fredWilkieLocation = new LatLong(47.6597, -122.3357);
+
+
+		//Set the initial properties of the map.
+		MapOptions mapOptions = new MapOptions();
+
+		mapOptions.center(new LatLong(47.6097, -122.3331))
+
+				.overviewMapControl(false)
+				.panControl(false)
+				.rotateControl(false)
+				.scaleControl(false)
+				.streetViewControl(false)
+				.zoomControl(false)
+				.zoom(12);
+
+		map = mapView.createMap(mapOptions);
+
+		//Add markers to the map
+		MarkerOptions markerOptions1 = new MarkerOptions();
+		markerOptions1.position(joeSmithLocation);
+
+		MarkerOptions markerOptions2 = new MarkerOptions();
+		markerOptions2.position(joshAndersonLocation);
+
+		MarkerOptions markerOptions3 = new MarkerOptions();
+		markerOptions3.position(bobUnderwoodLocation);
+
+		MarkerOptions markerOptions4 = new MarkerOptions();
+		markerOptions4.position(tomChoiceLocation);
+
+		MarkerOptions markerOptions5 = new MarkerOptions();
+		markerOptions5.position(fredWilkieLocation);
+
+		Marker joeSmithMarker = new Marker(markerOptions1);
+		Marker joshAndersonMarker = new Marker(markerOptions2);
+		Marker bobUnderwoodMarker = new Marker(markerOptions3);
+		Marker tomChoiceMarker= new Marker(markerOptions4);
+		Marker fredWilkieMarker = new Marker(markerOptions5);
+
+		map.addMarker( joeSmithMarker );
+		map.addMarker( joshAndersonMarker );
+		map.addMarker( bobUnderwoodMarker );
+		map.addMarker( tomChoiceMarker );
+		map.addMarker( fredWilkieMarker );
+
+	}
 }
